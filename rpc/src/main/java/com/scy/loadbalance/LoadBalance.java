5057a6f73387ad7ce2e1d46fa85c6279bc0224a4
si chenyong <sichenyongwork@163.com>
2025-02-05 17:39:40 +0800

    完成功能：实现常见的负载均衡算法，如随机策略、轮询、加权随机、加权轮询、源ip哈希算法

--- rpc/src/main/java/com/scy/loadbalance/LoadBalance.java
+++ rpc/src/main/java/com/scy/loadbalance/LoadBalance.java
@@ -4,14 +4,45 @@ import com.scy.comomon.URL;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Todo: 查看负载均衡算法具体是如何实现的
  */
 public class LoadBalance {
+    public static AtomicInteger index = new AtomicInteger(0);
+
+    /**
+     * 随机策略
+     * @param urls
+     * @return
+     */
     public static URL random(List<URL> urls) {
         Random random = new Random();
         int i = random.nextInt(urls.size());
+        return urls.get(i);
+    }
+
+    /**
+     * 轮询算法
+     * @param urls
+     * @return
+     */
+    public static URL RoundRobin(List<URL> urls) {
+        int size = urls.size();
+        int i = index.getAndIncrement() % size;
+        return urls.get(i);
+    }
+
+    /**
+     * 源ip 哈希算法
+     * @param urls
+     * @param ip
+     * @return
+     */
+    public static URL IpHash(List<URL> urls, String ip) {
+        int size = urls.size();
+        int i = ip.hashCode() % size;
         return urls.get(i);
     }
 }
